ecdde6ba4548a44f7b0ed33f06b578d851f9be7b
Capitalsav <capitalsav@gmail.com>
2016-07-04 11:25:55 +0300

    Json come as simple text to textView

--- TestRest2/app/src/main/java/com/example/adminhome/testrest/MainActivity.java
+++ TestRest2/app/src/main/java/com/example/adminhome/testrest/MainActivity.java
@@ -1,25 +1,94 @@
 package com.example.adminhome.testrest;
 
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.StringCharacterIterator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button getData = (Button) findViewById(R.id.getServiceData);
-        getData.setOnClickListener(new View.OnClickListener() {
+        Button btnHit = (Button) findViewById(R.id.btnHit);
+        tvData = (TextView)findViewById(R.id.tvJsonItem);
+
+        btnHit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String restURL = "http://www.androidexample.com/webservice/data/JSONReturn.php";
+                new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesDemoItem.txt");
             }
         });
-
     }
+
+    class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvData.setText(result);
+        }
+    }
+
 }
+
+
--- TestRest2/app/src/main/java/com/example/adminhome/testrest/RestOperation.java
+++
@@ -1,7 +0,0 @@
-package com.example.adminhome.testrest;
-
-/**
- * Created by AdminHome on 02.07.2016.
- */
-public class RestOperation {
-}
