b4904fc874b5f11ae608afad2bf59f3cbe9cb53d
Capitalsav <capitalsav@gmail.com>
2016-07-13 23:05:15 +0300

    added java code to taking full JSON objects with JSON arrays of JSON objects. Made custom view layout file.

--- TestRest2/app/src/main/java/com/example/adminhome/testrest/MainActivity.java
+++ TestRest2/app/src/main/java/com/example/adminhome/testrest/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -20,32 +23,30 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.StringCharacterIterator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvData;
+    private ListView lvMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnHit = (Button) findViewById(R.id.btnHit);
-        tvData = (TextView)findViewById(R.id.tvJsonItem);
+        lvMovies = (ListView) findViewById(R.id.lvMovies);
 
-        btnHit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesDemoItem.txt");
-                new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesDemoList.txt");
-            }
-        });
+
+        //new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesDemoItem.txt");
+        //new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesDemoList.txt");
     }
 
-    class JSONTask extends AsyncTask<String, String, String> {
+    class JSONTask extends AsyncTask<String, String, List<MovieModel>> {
 
         @Override
-        protected String doInBackground(String... params) {
+        protected List<MovieModel> doInBackground(String... params) {
             HttpURLConnection connection = null;
             BufferedReader reader = null;
 
@@ -70,17 +71,33 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject parentObject = new JSONObject(finalJSON);
                 JSONArray parentArray = parentObject.getJSONArray("movies");
 
-                StringBuffer finalBufferedData = new StringBuffer();
+                List<MovieModel> movieModelList = new ArrayList<>();
+
                 for(int i = 0; i < parentArray.length(); i++) {
                     JSONObject finalObject = parentArray.getJSONObject(i);
+                    MovieModel movieModel = new MovieModel();
+                    movieModel.setMovie(finalObject.getString("movie"));
+                    movieModel.setYear(finalObject.getInt("year"));
+                    movieModel.setRating((float) finalObject.getDouble("rating"));
+                    movieModel.setDirection(finalObject.getString("director"));
+                    movieModel.setDuration(finalObject.getString("duration"));
+                    movieModel.setImage(finalObject.getString("image"));
+                    movieModel.setStory(finalObject.getString("story"));
 
-                    String movieName = finalObject.getString("movie");
-                    int year = finalObject.getInt("year");
-
-                    finalBufferedData.append(movieName + " - " + year + "\n");
+                    List<MovieModel.Cast> castList = new ArrayList<>();
+                    for(int j = 0; j < finalObject.getJSONArray("cast").length(); j++) {
+                        //JSONObject castObject = finalObject.getJSONArray("cast").getJSONObject(j);
+                        MovieModel.Cast cast = new MovieModel.Cast();
+                        cast.setName(finalObject.getJSONArray("cast").getJSONObject(j).getString(
+                                "name"));
+                        castList.add(cast);
+                    }
+                    movieModel.setCastList(castList);
+                    //adding the final object in the list
+                    movieModelList.add(movieModel);
                 }
 
-                return finalBufferedData.toString();
+                return movieModelList;
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -104,12 +121,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String result) {
+        protected void onPostExecute(List<MovieModel> result) {
             super.onPostExecute(result);
-            tvData.setText(result);
+
+            //TODO need to set data to the list
         }
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            new JSONTask().execute("http://jsonparsing.parseapp.com/jsonData/moviesData.txt");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
 
 
